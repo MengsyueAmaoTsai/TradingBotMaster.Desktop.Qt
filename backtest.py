73d1307151442c05ad3fb7ebedf75bc97f780da1
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-01-14 22:34:18 +0800

    record to execution

---
+++ backtest.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from datetime import datetime
+from dataclasses import dataclass
+from trading_view import get_trading_view_records
+from enum import Enum
+
+records = get_trading_view_records(Path("data/trading-view/TV-PL-Binance_ETHUSDT.P-M15-0001_List_of_Trades_2024-01-13_14c27.csv"))
+
+
+class TradeType(Enum):
+    Buy = "Buy"
+    Sell = "Sell"
+
+@dataclass(frozen=True)
+class Execution():
+    """ """
+    time: datetime 
+    trade_type: TradeType
+    quantity: float
+    price: float
+
+for record in records:
+    trade_type = TradeType.Buy if record.type in ("Entry Long", "Exit Short") else TradeType.Sell
+
+    execution = Execution(time=record.time, trade_type=trade_type, quantity=record.quantity, price=record.price)
+
+    print(execution)
--- trading_view.py
+++ trading_view.py
@@ -3,12 +3,11 @@ from datetime import datetime
 from pathlib import Path
 TOTAL_COLUMNS = 14
 
-
 @dataclass(frozen=True)
 class TradingViewCsvRecord:
     trade_no: int
-    comment: str
     type: str
+    comment: str
     time: datetime
     price: float
     quantity: float
@@ -21,59 +20,41 @@ class TradingViewCsvRecord:
     drawdown: float
     drawdown_in_percentage: float
 
+    @classmethod
+    def from_raw_data(cls, raw_data: list[str]) -> "TradingViewCsvRecord":
+        return TradingViewCsvRecord(
+                trade_no=int(raw_data[0]), 
+                type=raw_data[1], 
+                comment=raw_data[2],
+                time=datetime.strptime(raw_data[3], "%Y-%m-%d %H:%M"),
+                price=float(raw_data[4]),
+                quantity=float(raw_data[5]),
+                realized_pnl=float(raw_data[6]),
+                realized_pnl_in_percent=float(raw_data[7]),
+                cumulative_pnl=float(raw_data[8]),
+                cumulative_pnl_in_percentage=float(raw_data[9]),
+                run_up=float(raw_data[10]),
+                run_up_in_percentage=float(raw_data[11]),
+                drawdown=float(raw_data[12]),
+                drawdown_in_percentage=float(raw_data[13]))
+
 file_path = Path("data/trading-view/TV-PL-Binance_ETHUSDT.P-M15-0001_List_of_Trades_2024-01-13_14c27.csv")
 
 def get_trading_view_records(path: Path) -> list[TradingViewCsvRecord]:
     records: list[TradingViewCsvRecord] = []
 
     with open(path, mode='r', encoding="utf-8") as raw_data:
-        
         rows: list[str] = raw_data.readlines()
-
-        # Remove header
         _ = rows.pop(0)
-        
         rows.reverse()
 
         for row in rows:
-            parts = row.split(',')
+            raw_data = row.split(',')
 
-            # Skip opening position record.
-            if (parts[3] == "" or parts[5] == ""):
+            if (raw_data[3] == "" or raw_data[5] == ""):
                 continue
-            
-            trade_no = int(parts[0])
-            comment = parts[1]
-            type = parts[2]
-            time = datetime.strptime(parts[3], "%Y-%m-%d %H:%M")
-            price = float(parts[4])
-            quantity = float(parts[5])
-            
-            realized_pnl = float(parts[6])
-            realized_pnl_in_percentage = float(parts[7])
-            cumulative_pnl = float(parts[8])
-            cumulative_pnl_in_percentage = float(parts[9])
-            run_up = float(parts[10])
-            run_up_in_percentage = float(parts[11])
-            drawdown = float(parts[12])
-            drawdown_in_percentage = float(parts[13])
 
-            record = TradingViewCsvRecord(
-                trade_no=trade_no, 
-                comment=comment, 
-                type=type,
-                time=time,
-                price=price,
-                quantity=quantity,
-                realized_pnl=realized_pnl,
-                realized_pnl_in_percent=realized_pnl_in_percentage,
-                cumulative_pnl=cumulative_pnl,
-                cumulative_pnl_in_percentage=cumulative_pnl_in_percentage,
-                run_up=run_up,
-                run_up_in_percentage=run_up_in_percentage,
-                drawdown=drawdown,
-                drawdown_in_percentage=drawdown_in_percentage)
-            records.append(record)
+            records.append(TradingViewCsvRecord.from_raw_data(raw_data))
 
     return records
 
