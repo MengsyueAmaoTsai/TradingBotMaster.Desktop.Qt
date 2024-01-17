09a5a0272c80b6cfbaa200dc499925db8bbb5ad3
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-01-18 01:46:09 +0800

    refactor application

--- src/__main__.py
+++ src/__main__.py
@@ -15,24 +15,24 @@ logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
 logger = logging.getLogger()
 
 
-class DesktopApplication:
-    def __init__(self) -> None:
+class DesktopApplication(QGuiApplication):
+    def __init__(self, args: list[str]) -> None:
+        super().__init__(args)
         version = get_version()
         app_qml_path = PROJECT_ROOT_PATH / SOURCE_DIR / APP_QML_NAME
 
-        self.__qt_app = QGuiApplication()
-        self.__qml_engine = QQmlApplicationEngine()
-
         # Set application information.
-        self.__qt_app.setOrganizationName("Richill Capital")
-        self.__qt_app.setOrganizationDomain("richillcapital.com")
-        self.__qt_app.setApplicationName(f"<applicationName> {get_project_name()}")
-        self.__qt_app.setApplicationDisplayName(f"<applicationDisplayName> {version}")
-        self.__qt_app.setApplicationVersion(version)
+        self.setOrganizationName("Richill Capital")
+        self.setOrganizationDomain("richillcapital.com")
+        self.setApplicationName(f"<applicationName> {get_project_name()}")
+        self.setApplicationDisplayName(f"<applicationDisplayName> {version}")
+        self.setApplicationVersion(version)
 
         # Load the main qml file.
+        self.__qml_engine = QQmlApplicationEngine()
         self.__qml_engine.load(app_qml_path)
 
+        # Ensure the application loaded successfully.
         if not self.__qml_engine.rootObjects():
             logger.error("Application failed to load.")
             sys.exit(-1)
@@ -42,7 +42,7 @@ class DesktopApplication:
         return DesktopApplicationBuilder(args)
 
     def run(self) -> None:
-        sys.exit(self.__qt_app.exec())
+        sys.exit(self.exec())
 
 
 class DesktopApplicationBuilder:
@@ -54,10 +54,9 @@ class DesktopApplicationBuilder:
         if ("--reload" in self.__args) or ("-r" in self.__args):
             logger.info("Enable hot reload")
 
-        app = DesktopApplication()
+        app = DesktopApplication(self.__args)
         return app
 
 
-builder = DesktopApplication.create_builder(sys.argv)
-app = builder.build()
+app = DesktopApplication.create_builder(sys.argv).build()
 app.run()
