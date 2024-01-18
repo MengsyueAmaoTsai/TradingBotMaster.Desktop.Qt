19b71549b40f3de335286fca8eb1d9acb2582257
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-01-18 21:41:42 +0800

    Move IEnvironment to abstraction.py

--- src/qtpy/environments.py
+++ src/qtpy/environments.py
@@ -1,36 +1,8 @@
 from enum import Enum
 from pathlib import Path
-from typing import Protocol, override
+from typing import override
 
-
-class IEnvironment(Protocol):
-    """Environment interface."""
-
-    @property
-    def content_root_path(self) -> Path:
-        """Return the content root path."""
-        ...
-
-    @property
-    def assets_path(self) -> str:
-        """Return the assets path."""
-        ...
-
-    def is_development(self) -> bool:
-        """Return True if the environment is development."""
-        ...
-
-    def is_staging(self) -> bool:
-        """Return True if the environment is staging."""
-        ...
-
-    def is_production(self) -> bool:
-        """Return True if the environment is production."""
-        ...
-
-    @override
-    def __str__(self) -> str:
-        ...
+from .abstractions import IEnvironment
 
 
 class Environments(str, Enum):
