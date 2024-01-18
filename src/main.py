969288c778b5d22b45d2e5afa79d64b87c8df7b5
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-01-19 00:04:33 +0800

    Enable GC

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
+import gc
 import sys
 
 from qtpy import DesktopApplication
 from view_models import MainViewModel
 
 if __name__ == "__main__":
+    gc.set_debug(gc.DEBUG_STATS)
+
     builder = DesktopApplication.create_builder(sys.argv)
 
     view_model = MainViewModel()
