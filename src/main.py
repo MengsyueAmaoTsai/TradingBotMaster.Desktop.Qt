98d0f01b8985fde929a35391300f2e9235bb9b46
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-01-18 14:07:23 +0800

    fix import error

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
-from .qtpy import DesktopApplication
+
+from qtpy import DesktopApplication
 
 if __name__ == "__main__":
     app = DesktopApplication.create_builder(sys.argv).build()
--- src/qtpy/__init__.py
+++ src/qtpy/__init__.py
@@ -101,3 +101,6 @@ class DesktopApplicationBuilder:
         )
 
         return app
+
+
+__all__ = ["DesktopApplication", "DesktopApplicationBuilder"]
